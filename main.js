9f34a2b6b6df4dda330089d75e5a63e0cd46fe78
SamynathanS6720 <91730610+SamynathanS6720@users.noreply.github.com>
2023-05-23 15:52:11 +0530

    level-5

--- main.js
+++ main.js
@@ -4,8 +4,19 @@ const app = Vue.createApp({
             // Product
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
-            inStock: 0 ,
-            onSale: true
+            inStock: 0,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants:[
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' },
+            ],
+            sizes:[
+                { id: 1134, size: '10' },
+                { id: 1135, size: '9' },
+                { id: 1136, size: '8' },
+                { id: 1137, size: '7' },
+                { id: 1138, size: '6' },
+            ]
         }
     }
 })
