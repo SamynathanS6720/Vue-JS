7acbefb4d6ba20b47d5eb893bd37a2ef0d3494e7
SamynathanS6720 <91730610+SamynathanS6720@users.noreply.github.com>
2023-05-23 16:31:01 +0530

    level-6

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 const app = Vue.createApp({
     data(){
         return{
-            // Product
+            cart: 0,
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
             inStock: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants:[
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', },
+                { id: 2235, color: 'blue' , image: './assets/images/socks_blue.jpg', },
             ],
-            sizes:[
-                { id: 1134, size: '10' },
-                { id: 1135, size: '9' },
-                { id: 1136, size: '8' },
-                { id: 1137, size: '7' },
-                { id: 1138, size: '6' },
-            ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        removeFromCart() {
+            this.cart -=1
+        },
+        updateImage( variantImage ){
+            this.image= variantImage
         }
     }
 })
