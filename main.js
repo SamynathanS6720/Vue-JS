8d9f6393f05f87117340fd42b44f7ad74dffeb32
SamynathanS6720 <91730610+SamynathanS6720@users.noreply.github.com>
2023-05-24 18:11:26 +0530

    level-8

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ const app = Vue.createApp({
         return{
             cart: 0,
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            inStock: 100,
+            brand: 'Ibaco',
+            selectedVariant: 0, 
+            // image: './assets/images/socks_green.jpg',
+            // inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants:[
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', },
-                { id: 2235, color: '#00008B' , image: './assets/images/socks_blue.jpg', },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: '#00008B' , image: './assets/images/socks_blue.jpg', quantity: 0 },
             ],
         }
     },
@@ -19,8 +21,22 @@ const app = Vue.createApp({
         removeFromCart() {
             this.cart -=1
         },
-        updateImage( variantImage ){
-            this.image= variantImage
+        updateVariant( index ){
+            this.selectedVariant= index
+        }
+    },
+    computed:{
+        title(){
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        sale() {
+            return this.brand + ' ' + this.product + ' is an sale!!'
         }
     }
 })
